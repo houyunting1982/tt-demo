0d42af5bb02445c825cab3a414b560fe1da68eb6
Yunting Hou <yhou@servicetitan.com>
2021-12-05 23:52:24 -0800

    Fix wrong reference

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./pages/Home";
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CoachBio from "./pages/CoachBio";
 import Tournament from "./pages/Tournament";
 import TournamentsBoard from "./pages/TournamentsBoard";
@@ -9,12 +9,12 @@ import TournamentsBoard from "./pages/TournamentsBoard";
 function App() {
     return (
         <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Switch>
+            <Routes>
                 <Route path='/' exact element={<Home />} />
                 <Route path='/coaches/:name' element={<CoachBio />} />
                 <Route path='/tournament' element={<TournamentsBoard />} />
                 <Route path='/tournament/:id' element={<Tournament />} />
-            </Switch>
+            </Routes>
         </BrowserRouter>
     );
 }
